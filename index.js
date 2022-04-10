a151ef9d4e7ef4eba95fec63fa9464d29c19ac0b
SATGANZBOTZ <natan.aja216@gmail.com>
2022-04-10 21:08:12 +0700

    Satganz-MDV2

---
+++ index.js
@@ -0,0 +1,149 @@
+/**
+  * Edit features in './message/msg.js'
+  * Contact me on WhatsApp wa.me/6285791458996
+  * Follow https://github.com/rtwone
+  * If you want to buy an updated script that is not encrypted, please WhatsApp me
+*/
+
+"use strict";
+const {
+	default: makeWASocket,
+	BufferJSON,
+	initInMemoryKeyStore,
+	DisconnectReason,
+	AnyMessageContent,
+        makeInMemoryStore,
+	useSingleFileAuthState,
+	delay
+} = require("@adiwajshing/baileys")
+const figlet = require("figlet");
+const fs = require("fs");
+const moment = require('moment')
+const chalk = require('chalk')
+const logg = require('pino')
+const clui = require('clui')
+const { Spinner } = clui
+const { serialize } = require("./lib/myfunc");
+const { color, mylog, infolog } = require("./lib/color");
+const time = moment(new Date()).format('HH:mm:ss DD/MM/YYYY')
+let setting = JSON.parse(fs.readFileSync('./config.json'));
+let session = `./${setting.sessionName}.json`
+const { state, saveState } = useSingleFileAuthState(session)
+let welcome = JSON.parse(fs.readFileSync('./database/welcome.json'));
+
+function title() {
+      console.clear()
+	  console.log(chalk.bold.green(figlet.textSync('WaBot MD', {
+		font: 'Standard',
+		horizontalLayout: 'default',
+		verticalLayout: 'default',
+		width: 80,
+		whitespaceBreak: false
+	})))
+	console.log(chalk.yellow(`\n                        ${chalk.yellow('[ Created By Irfan ]')}\n\n${chalk.red('Chitanda Eru Bot')} : ${chalk.white('WhatsApp Bot Multi Device')}\n${chalk.red('Follow Insta Dev')} : ${chalk.white('@yannnnn.zz_')}\n${chalk.red('Message Me On WhatsApp')} : ${chalk.white('+62 857-9145-8996')}\n${chalk.red('Donate')} : ${chalk.white('085791458996 ( Gopay/Pulsa )')}\n`))
+}
+
+/**
+* Uncache if there is file change;
+* @param {string} module Module name or path;
+* @param {function} cb <optional> ;
+*/
+function nocache(module, cb = () => { }) {
+	console.log(`Module ${module} sedang diperhatikan terhadap perubahan`) 
+	fs.watchFile(require.resolve(module), async () => {
+		await uncache(require.resolve(module))
+		cb(module)
+	})
+}
+/**
+* Uncache a module
+* @param {string} module Module name or path;
+*/
+function uncache(module = '.') {
+	return new Promise((resolve, reject) => {
+		try {
+			delete require.cache[require.resolve(module)]
+			resolve()
+		} catch (e) {
+			reject(e)
+		}
+	})
+}
+
+const status = new Spinner(chalk.cyan(` Booting WhatsApp Bot`))
+const starting = new Spinner(chalk.cyan(` Preparing After Connect`))
+const reconnect = new Spinner(chalk.redBright(` Reconnecting WhatsApp Bot`))
+
+const store = makeInMemoryStore({ logger: logg().child({ level: 'fatal', stream: 'store' }) })
+
+const connectToWhatsApp = async () => {
+	const conn = makeWASocket({
+            printQRInTerminal: true,
+            logger: logg({ level: 'fatal' }),
+            auth: state,
+            browser: ["Satganz Multi Device, "Safari", "3.0"]
+        })
+	title()
+        store.bind(conn.ev)
+	
+	/* Auto Update */
+	require('./message/help')
+	require('./lib/myfunc')
+	require('./message/msg')
+	nocache('./message/help', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+	nocache('./lib/myfunc', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+	nocache('./message/msg', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+	
+	conn.multi = true
+	conn.nopref = false
+	conn.prefa = 'anjing'
+	conn.ev.on('messages.upsert', async m => {
+		if (!m.messages) return;
+		var msg = m.messages[0]
+		msg = serialize(conn, msg)
+		msg.isBaileys = msg.key.id.startsWith('BAE5') || msg.key.id.startsWith('3EB0')
+		require('./message/msg')(conn, msg, m, setting, store, welcome)
+	})
+	conn.ev.on('connection.update', (update) => {
+		const { connection, lastDisconnect } = update
+		if (connection === 'close') {
+			status.stop()
+			reconnect.stop()
+			starting.stop()
+			console.log(mylog('Server Ready ✓'))
+			lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut 
+			? connectToWhatsApp()
+			: console.log(mylog('Wa web terlogout...'))
+		}
+	})
+	conn.ev.on('creds.update', () => saveState)
+	
+        conn.ev.on('group-participants.update', async (data) => {
+          const isWelcome = welcome.includes(data.id) ? true : false
+          if (isWelcome) {
+            try {
+              for (let i of data.participants) {
+                try {
+                  var pp_user = await conn.profilePictureUrl(i, 'image')
+                } catch {
+                  var pp_user = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+                }
+                if (data.action == "add") {
+                  conn.sendMessage(data.id, { image: { url: pp_user }, caption: `Welcome @${i.split("@")[0]}`, mentions: [i] })
+                } else if (data.action == "remove") {
+                  conn.sendMessage(data.id, { image: { url: pp_user }, caption: `Sayonara @${i.split("@")[0]}`, mentions: [i] })
+                }
+              }
+            } catch (e) {
+              console.log(e)
+            }
+          }
+        })
+
+	conn.reply = (from, content, msg) => conn.sendMessage(from, { text: content }, { quoted: msg })
+
+	return conn
+}
+
+connectToWhatsApp()
+.catch(err => console.log(err))
---
+++ message/help.js
@@ -0,0 +1,105 @@
+const moment = require("moment-timezone");
+const fs = require("fs");
+
+moment.tz.setDefault("Asia/Jakarta").locale("id");
+
+let dt = moment(Date.now()).tz('Asia/Jakarta').locale('id').format('a')
+const ucapanWaktu = "Selamat "+dt.charAt(0).toUpperCase() + dt.slice(1)
+let setting = JSON.parse(fs.readFileSync('./config.json'))
+const { getLimit, getBalance, cekGLimit } = require("../lib/limit")
+
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+
+function toCommas(x) {
+	x = x.toString()
+	var pattern = /(-?\d+)(\d{3})/;
+     while (pattern.test(x))
+	   x = x.replace(pattern, "$1,$2");
+	return x;
+}
+
+exports.allmenu = (sender, prefix, pushname, isOwner, isPremium, balance, limit, limitCount, glimit, gcount) => {
+	return `*── 「 ${setting.botName} - MD Beta 」 ──*
+	
+  _*${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}*_
+
+    Library : *Baileys-MD*.
+    Prefix : ( ${prefix} )
+    Tanggal Server : ${moment.tz('Asia/Jakarta').format('DD/MM/YY')}
+    Waktu Server : ${moment.tz('Asia/Jakarta').format('HH:mm:ss')}
+
+	Status : ${isOwner ? 'Owner' : isPremium ? 'Premium' : 'Free'}
+	Limit Harian : ${isOwner ? '-' : isPremium ? 'Unlimited' : getLimit(sender, limitCount, limit)}
+	Limit Game : ${isOwner ? '-' : cekGLimit(sender, gcount, glimit)}
+	Balance : $${toCommas(getBalance(sender, balance))}
+
+  *( 📍 )  Main Menu*
+  ≻ ${prefix}menu
+  ≻ ${prefix}owner
+  ≻ ${prefix}donasi
+  ≻ ${prefix}speed
+  ≻ ${prefix}runtime
+  ≻ ${prefix}cekprem
+  ≻ ${prefix}listprem
+
+  *( ✏️ )  Converter/Tools*
+  ≻ ${prefix}sticker
+  ≻ ${prefix}toimg
+  ≻ ${prefix}tovid
+
+  *( ⌛ )  Downloader*
+  ≻ ${prefix}play
+  ≻ ${prefix}tiktok
+  ≻ ${prefix}ytmp4
+  ≻ ${prefix}ytmp3
+  ≻ ${prefix}getvideo
+  ≻ ${prefix}getmusic
+  ≻ ${prefix}instagram
+  ≻ ${prefix}facebook
+  
+  *( ♻️ )  Random Menu*
+  ≻ ${prefix}quote
+  ≻ ${prefix}cecan
+  ≻ ${prefix}cogan
+  
+  *( 🔎 )  Search Menu*
+  ≻ ${prefix}lirik
+  ≻ ${prefix}grupwa
+  ≻ ${prefix}ytsearch
+  
+  *( 🎮 )  Game Menu*
+  ≻ ${prefix}tictactoe
+  ≻ ${prefix}delttc
+  ≻ ${prefix}tebakgambar
+  
+  *( 🏦 )  Payment & Bank*
+  ≻ ${prefix}buylimit
+  ≻ ${prefix}buyglimit
+  ≻ ${prefix}transfer
+  ≻ ${prefix}limit
+  ≻ ${prefix}balance
+  
+  *( 👥 )  Group Menu*
+  ≻ ${prefix}linkgrup
+  ≻ ${prefix}setppgrup
+  ≻ ${prefix}setnamegc
+  ≻ ${prefix}setdesc
+  ≻ ${prefix}group
+  ≻ ${prefix}revoke
+  ≻ ${prefix}hidetag
+  
+  *( 🧑🏻‍💻 )  Owner Menu*
+  > evalcode
+  x evalcode-2
+  $ executor
+  ≻ ${prefix}join
+  ≻ ${prefix}broadcast
+  ≻ ${prefix}setppbot
+  ≻ ${prefix}exif
+  ≻ ${prefix}leave
+  ≻ ${prefix}addprem
+  ≻ ${prefix}delprem
+
+`+'     \`\`\`Powered by Nodejs\`\`\`'
+}
